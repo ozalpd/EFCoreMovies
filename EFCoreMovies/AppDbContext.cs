c91a6d076df51f6a9e93c9c58813c38203030e49
Özalp Döndüren <ozalp@donduren.com>
2022-05-07 10:31:47 +0300

    Add project files.

---
+++ EFCoreMovies/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
