301fdd36f95448f8b44e8732a5b1f83cabcbb511
Özalp Döndüren <ozalp@donduren.com>
2022-05-08 17:51:12 +0300

    Added GenresController

---
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreMovies.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
+    {
+        public GenresController(AppDbContext context)
+        {
+            dbContext = context;
+        }
+        private readonly AppDbContext dbContext;
+
+        [HttpGet]
+        public async Task<IEnumerable<Genre>> Get()
+        {
+            return await dbContext.Genres.ToListAsync();
+        }
+    }
+}
