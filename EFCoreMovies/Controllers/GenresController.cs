41dab41b4019392ee07734b9f3017e78e2f23831
Özalp Döndüren <ozalpd@outlook.com>
2022-05-21 19:27:13 +0300

    Added search and pagination to Genres/Get

--- EFCoreMovies/Controllers/GenresController.cs
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -15,11 +15,26 @@ namespace EFCoreMovies.Controllers
         private readonly AppDbContext dbContext;
 
         [HttpGet]
-        public async Task<IEnumerable<Genre>> Get()
+        public async Task<IEnumerable<Genre>> Get(string searchString, int page = 1, int pageSize = 2)
         {
-            return await dbContext.Genres
-                                  //.AsNoTracking()
-                                  .ToListAsync();
+            IQueryable<Genre> query = dbContext.Genres;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                query = from g in query
+                        where g.Name.Contains(searchString)
+                           || g.Movies.Any(m => m.Title.Contains(searchString))
+                        select g;
+                //.Where(g => );
+            }
+
+            //int recCount = await query.CountAsync();
+            page = page < 1 ? 1 : page;
+            pageSize = pageSize < 1 ? 10 : pageSize;
+            return await query.OrderBy(g => g.Name)
+                              .Skip((page - 1) * pageSize)
+                              .Take(pageSize)
+                              .AsNoTracking()
+                              .ToListAsync();
         }
     }
 }
