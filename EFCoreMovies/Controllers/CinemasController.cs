fc004bdf4cc504c4cca7d4af570aa059636c3cb3
Özalp Döndüren <ozalpd@outlook.com>
2022-05-21 22:25:36 +0300

    Mapped spatial data of cinemas to Latitude + Longitude properties of CinemaDTO

---
+++ EFCoreMovies/Controllers/CinemasController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EFCoreMovies.Entities;
+using EFCoreMovies.Entities.DTOs;
+using EFCoreMovies.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/Cinemas")]
+    public class CinemasController : AbstractMapperController
+    {
+        public CinemasController(AppDbContext context, IMapper mapper) : base(context, mapper) { }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<CinemaDTO>> Get(string searchString, int page = 1, int pageSize = 2)
+        {
+            IQueryable<Cinema> query = dbContext.Cinemas;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                query = query.Where(a => a.Name.Contains(searchString));
+            }
+
+            int recCount = await query.CountAsync();
+            var dtoQuery = query.OrderBy(a => a.Name)
+                                .Paginate(page, pageSize, recCount)
+                                .ProjectTo<CinemaDTO>(mapper.ConfigurationProvider);
+            return await dtoQuery.ToListAsync();
+        }
+    }
+}
---
+++ EFCoreMovies/Entities/DTOs/CinemaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreMovies.Entities.DTOs
+{
+    public class CinemaDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+
+        public decimal TicketPrice { get; set; }
+    }
+}
